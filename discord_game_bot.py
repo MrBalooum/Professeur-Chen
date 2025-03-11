279446fad2feb9352c78fd2b5eb82ca42aba8656
MrBalooum <mrbalooum@gmail.com>
2025-03-11 15:06:28 +0100

    Update discord_game_bot.py

--- discord_game_bot.py
+++ discord_game_bot.py
@@ -21,12 +21,12 @@ BOOSTERS = {
         "6": {
             "drop_rate": 0.02083,
             "image_url": "https://raw.githubusercontent.com/MrBalooum/Professeur-Chen/refs/heads/Pokemon-Card/6.png",
-            "allowed_positions" = [1, 2, 3, 4]  
+            "allowed_positions" : [1, 2, 3, 4]  
         },
         "39": {
             "drop_rate": 0.02083,
             "image_url": "https://raw.githubusercontent.com/MrBalooum/Professeur-Chen/refs/heads/Pokemon-Card/39.png",
-            "allowed_positions" = [1, 2, 3, 4] 
+            "allowed_positions" : [1, 2, 3, 4] 
         },
           "42": {
             "drop_rate": 0.02083,
