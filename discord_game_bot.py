c0cac86c4bbbde671eacc74da6d1bb2199defa1e
MrBalooum <mrbalooum@gmail.com>
2025-03-13 16:15:52 +0100

    Update discord_game_bot.py

--- discord_game_bot.py
+++ discord_game_bot.py
@@ -1247,15 +1247,18 @@ async def booster(interaction: discord.Interaction, nom: str):
         eligible_cards = [card for card, data in cards.items() if position in data["allowed_positions"]]
         if eligible_cards:
             weights = [cards[card]["drop_rate"] for card in eligible_cards]
-            print(f"Position {position}: Cartes éligibles = {eligible_cards}, Poids = {weights}")
             selected_card = random.choices(eligible_cards, weights=weights)[0]
             selected_cards.append(selected_card)
-            print(f"Carte sélectionnée pour la position {position}: {selected_card}")
         else:
             # Si aucune carte n'est éligible pour cette position, sélectionner une carte aléatoire parmi toutes les cartes
             selected_card = random.choices(list(cards.keys()), weights=[data["drop_rate"] for data in cards.values()])[0]
             selected_cards.append(selected_card)
-            print(f"Aucune carte éligible pour la position {position}, carte sélectionnée: {selected_card}")
+
+    # Insérer les cartes sélectionnées dans la collection de l'utilisateur
+    user_id = interaction.user.id
+    for card_name in selected_cards:
+        cursor.execute('INSERT OR IGNORE INTO user_collections (user_id, card_name) VALUES (?, ?)', (user_id, card_name))
+    conn.commit()
 
     # URL de l'image du booster en fonction du nom du booster
     if nom == "PGO - Pokemon Go":
